788dfdb92e7f7f476ef3b3ce20cd0a298ddeaa90
javohirfrontenddevs <javohirabdusharipov676@gmail.com>
2025-06-03 16:31:01 +0500

    added swagger configuration

--- apps/api_gateway/src/swagger/swagger.config.ts
+++ apps/api_gateway/src/swagger/swagger.config.ts
@@ -1,22 +1,22 @@
 export const services = [
   {
     name: "auth_service",
-    url: "https://e-commerce-backend-2-xcpq.onrender.com/swagger-json",
+    url: "https://e-commerce-backend-2-xcpq.onrender.com/api-docs",
   },
   {
     name: "order_service",
-    url: "https://tezbuy-order-service-backend.onrender.com/swagger-json",
+    url: "https://tezbuy-order-service-backend.onrender.com/api-docs",
   },
   {
     name: "products_service",
-    url: "https://tezbuy-products-service-backend.onrender.com/swagger-json",
+    url: "https://tezbuy-products-service-backend.onrender.com/api-docs",
   },
   // {
   //   name: "punkt_service",
-  //   url: "http://localhost:4004/swagger-json",
+  //   url: "http://localhost:4004/api-docs",
   // },
   // {
   //   name: "staff_service",
-  //   url: "http://localhost:4005/swagger-json",
+  //   url: "http://localhost:4005/api-docs",
   // },
 ];
