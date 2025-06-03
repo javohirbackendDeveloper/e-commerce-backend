90dac9b9653d0ee1bfab53bf202f692ef643d586
javohirfrontenddevs <javohirabdusharipov676@gmail.com>
2025-06-03 13:29:23 +0500

    deleted https agent

--- apps/api_gateway/src/api-gateway.controller.ts
+++ apps/api_gateway/src/api-gateway.controller.ts
@@ -164,9 +164,7 @@ export class ApiGatewayController {
           .status(500)
           .json({ message: "Target service not configured" });
       }
-      const httpsAgent = new https.Agent({
-        rejectUnauthorized: false,
-      });
+
       const response = await lastValueFrom(
         this.httpService.request({
           method,
@@ -176,11 +174,8 @@ export class ApiGatewayController {
             ...headers,
             x_allowed_origin: process.env.API_GATEWAY_URL,
           },
-          httpsAgent,
         })
       );
-
-      console.log({ response });
 
       if (response.headers["set-cookie"]) {
         res.setHeader("Set-Cookie", response.headers["set-cookie"]);
