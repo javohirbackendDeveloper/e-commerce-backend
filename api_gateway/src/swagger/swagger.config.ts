b18304b5f22666d597c7f227e6581db9d740eadd
javohirfrontenddevs <javohirabdusharipov676@gmail.com>
2025-07-02 10:39:10 +0500

    changed products_service endpoint

--- api_gateway/src/swagger/swagger.config.ts
+++ api_gateway/src/swagger/swagger.config.ts
@@ -5,7 +5,7 @@ export const services = [
   },
   {
     name: "products_service",
-    url: "https://tezbuy-products-service-backend.onrender.com/swagger-json",
+    url: "https://tezbuy-product-service-backend.onrender.com/swagger-json",
   },
   {
     name: "order_service",
